231dc0c6aecb98997ad79aede2a5d8b2e07942a4
annagaiel <annagaiel@gmail.com>
2015-11-21 11:35:52 -0800

    Fix the split to three only and Fix regex on sub_count

--- discre.rb
+++ discre.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'csv'
+require 'pry'
 
 csv1 = ARGV[0]
 csv2 = ARGV[1]
@@ -11,13 +12,15 @@ data2 = CSV.readlines(ARGV[1], :headers => true)
 results = []
 
 (0...data1.size).each do |index|
-  temp_data_1 = data1[index].to_s.split(',')
-  temp_data_2 = data2[index].to_s.split(',')
+  temp_data_1 = data1[index].to_s.split(',', 3)
+  temp_data_2 = data2[index].to_s.split(',', 3)
 
   if temp_data_1[1].gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '') != temp_data_2[1].gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '')
-     results << temp_data_1[0] if concern == "channel_ownership" || concern.to_s.empty?
-  elsif temp_data_1[2].gsub(/\,/,'') != temp_data_2[2].gsub(/\,/,'')
-     results << temp_data_1[0] if concern == "subscriber_count" || concern.to_s.empty?
+     results << temp_data_1[0] if concern == "channel_ownership" || concern.to_s.empty? && !results.include?(temp_data_1[0])
+  end
+
+  if temp_data_1[2].gsub(/[^0-9]/, '') != temp_data_2[2].gsub(/[^0-9]/, '')
+     results << temp_data_1[0] if concern == "subscriber_count" || concern.to_s.empty? && !results.include?(temp_data_1[0])
   end
 
 end
