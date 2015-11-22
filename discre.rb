33152bd7aee7143f87398190a0dd09dd7c9fb451
annagaiel <annagaiel@gmail.com>
2015-11-22 06:17:46 -0800

    Make code readable via youtube_difference, subcriber_difference, and check_concern

--- discre.rb
+++ discre.rb
@@ -6,21 +6,38 @@ csv1 = ARGV[0]
 csv2 = ARGV[1]
 concern = ARGV[2]
 
-data1 = CSV.readlines(ARGV[0], :headers => true)
-data2 = CSV.readlines(ARGV[1], :headers => true)
+data1 = CSV.readlines(csv1, :headers => true)
+data2 = CSV.readlines(csv2, :headers => true)
 
 results = []
 
-(0...data1.size).each do |index|
-  temp_data_1 = data1[index].to_s.split(',', 3)
-  temp_data_2 = data2[index].to_s.split(',', 3)
+# Returns true if youtube_channel_1 is not equal to youtube_channel_2
+# Removes the youtube site and UC
+def youtube_difference(youtube_channel_1, youtube_channel_2)
+  youtube_channel_1.gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '') != youtube_channel_2.gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '')
+end
 
-  if temp_data_1[1].gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '') != temp_data_2[1].gsub(/^(https?\:\/\/(www.)?)?youtube.com\/channel\//, '').gsub(/^UC/, '')
-     results << temp_data_1[0] if concern == "channel_ownership" || concern.to_s.empty? && !results.include?(temp_data_1[0])
+# Returns true if subscriber_count_1 is not equal to subscriber_count_2
+# Removes the non numerical characters such as quotes and commas
+def subcriber_difference(subscriber_count_1, subscriber_count_2)
+  subscriber_count_1.gsub(/[^0-9]/, '') != subscriber_count_2.gsub(/[^0-9]/, '')
+end
+
+# Returns true if email is not in results and concern is emtpy or is equal to channel_ownership or  subscriber_count
+def check_concern(concern, results, arg, email)
+  concern == arg || concern.to_s.empty? && !results.include?(email)
+end
+
+(0...data1.size).each do |index|
+  row_1 = data1[index]
+  row_2 = data2[index]
+
+  if youtube_difference(row_1[" YouTube Channel"], row_2[" YouTube Channel"]) && check_concern(concern, results, "channel_ownership", row_1["Account Email"])
+      results << row_1["Account Email"]
   end
 
-  if temp_data_1[2].gsub(/[^0-9]/, '') != temp_data_2[2].gsub(/[^0-9]/, '')
-     results << temp_data_1[0] if concern == "subscriber_count" || concern.to_s.empty? && !results.include?(temp_data_1[0])
+  if subcriber_difference(row_1[" Subscriber Count"], row_2[" Subscriber Count"]) && check_concern(concern, results, "subscriber_count", row_2["Account Email"])
+      results << row_1["Account Email"]
   end
 
 end
